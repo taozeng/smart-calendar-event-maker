491f5a443c2b8e4f4c1149029bb9189165fa47d7
Tao Zeng <tao.zeng@ihsmarkit.com>
2018-10-12 21:46:35 -0700

    Check boundary for address match.

--- extractor/getAddress.js
+++ extractor/getAddress.js
@@ -1,4 +1,4 @@
-var addressMatch=/[1-9]\d{2,4}\s+(\S+\s+){2,9}\d{5}/igm;
+var addressMatch=/\b[1-9]\d{2,4}\s+(\S+\s+){2,9}\d{5}\b/igm;
 
 function getAddress(text) {
     var addrArry=text.match(addressMatch);
