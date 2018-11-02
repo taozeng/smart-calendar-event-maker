cf01d03bdc0669031f7aa48847608147dcfb8e4d
Tao Zeng <tao.zeng@ihsmarkit.com>
2018-11-01 23:02:35 -0700

    Add document title for all events.

--- content.js
+++ content.js
@@ -8,51 +8,51 @@ var additionalInfo = {
 function getAdditionalInfo() {
 
   additionalInfo.date = "None";
-  additionalInfo.title = "None";
+  additionalInfo.title = document.title;
   if (document.URL.indexOf("mail.google.com") > -1) {
 
     //var 
     var dateString = document.getElementsByClassName('g3')[0].title;
-    dateString = dateString.substring(5, 17);
-    dateString = dateString.replace(",", "");
+    // dateString = dateString.substring(5, 17);
+    // dateString = dateString.replace(",", "");
     additionalInfo.date = dateString;
     additionalInfo.title = document.title;
   }
-  else if (document.URL.indexOf("mail.live.com") > -1) {
-    additionalInfo.date = document.getElementsByClassName('Date TextSizeSmall')[0].innerHTML;
-    additionalInfo.date = additionalInfo.date.trim();
+  // else if (document.URL.indexOf("mail.live.com") > -1) {
+  //   additionalInfo.date = document.getElementsByClassName('Date TextSizeSmall')[0].innerHTML;
+  //   additionalInfo.date = additionalInfo.date.trim();
 
-    //window.alert(additionalInfo.date);
-    if (additionalInfo.date.length < 8) {
+  //   //window.alert(additionalInfo.date);
+  //   if (additionalInfo.date.length < 8) {
 
-      var temp = new Date();
-      tempMonth = temp.getMonth() + 1;
-      tempDate = temp.getDate();
-      if (tempMonth.toString().length < 2) {
-        tempMonth = "0" + tempMonth;
-      }
-      if (tempDate.toString().length < 2) {
-        tempDate = "0" + tempDate;
-      }
-      additionalInfo.date = tempDate + "/" + tempMonth + "/" + temp.getFullYear();
-    }
+  //     var temp = new Date();
+  //     tempMonth = temp.getMonth() + 1;
+  //     tempDate = temp.getDate();
+  //     if (tempMonth.toString().length < 2) {
+  //       tempMonth = "0" + tempMonth;
+  //     }
+  //     if (tempDate.toString().length < 2) {
+  //       tempDate = "0" + tempDate;
+  //     }
+  //     additionalInfo.date = tempDate + "/" + tempMonth + "/" + temp.getFullYear();
+  //   }
 
-    additionalInfo.title = document.getElementsByClassName('rmSubject')[0].innerHTML;
-    additionalInfo.title = additionalInfo.title.trim();
-    var i = additionalInfo.title.length - 35;
-    if (i > 34) {
-      additionalInfo.title = additionalInfo.title.substring(34, i);
-    }
-  }
-  else if (document.URL.indexOf("mail.yahoo.com") > -1) {
+  //   additionalInfo.title = document.getElementsByClassName('rmSubject')[0].innerHTML;
+  //   additionalInfo.title = additionalInfo.title.trim();
+  //   var i = additionalInfo.title.length - 35;
+  //   if (i > 34) {
+  //     additionalInfo.title = additionalInfo.title.substring(34, i);
+  //   }
+  // }
+  // else if (document.URL.indexOf("mail.yahoo.com") > -1) {
 
-    var j = document.getElementsByClassName('short').length;
+  //   var j = document.getElementsByClassName('short').length;
 
-    additionalInfo.date = document.getElementsByClassName('short')[j - 1].title;
-    //window.alert(additionalInfo.date);
-    j = document.getElementsByClassName('thread-subject').length;
-    additionalInfo.title = document.getElementsByClassName('thread-subject')[j - 1].title;
-  }
+  //   additionalInfo.date = document.getElementsByClassName('short')[j - 1].title;
+  //   //window.alert(additionalInfo.date);
+  //   j = document.getElementsByClassName('thread-subject').length;
+  //   additionalInfo.title = document.getElementsByClassName('thread-subject')[j - 1].title;
+  // }
 
   return additionalInfo;
 }
