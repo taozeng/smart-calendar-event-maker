1a11ad54c73e6f32274eb1754581bdcb16668793
Tao Zeng <tao.zeng@ihsmarkit.com>
2018-11-02 21:13:06 -0700

    Add supporting Canadian address.

--- extractor/getAddress.js
+++ extractor/getAddress.js
@@ -1,7 +1,9 @@
-var addressMatch = /\b[1-9]\d{2,4}\s+(\S+\s+){2,9}\d{5}\b/igm;
+var addressMatchUS = /\b[1-9]\d{2,4}\s+(\S+\s+){2,9}\d{5}\b/igm;
+var addressMatchCA = /\b[1-9]\d{1,4}\s+(\S+\s+){2,9}[ABCEGHJKLMNPRSTVXY]\d[A-Z][ -]\d[A-Z]\d\b/igm;
 
 function getAddress(text) {
-    var addrArry = text.match(addressMatch);
-    //console.log(addrArry);
+    var addrArry = text.match(addressMatchUS);
+    if (addrArry == null)
+        addrArry = text.match(addressMatchCA);
     return addrArry;
 }
